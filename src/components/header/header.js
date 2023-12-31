5d7a903feb41b6288dd5833a07a5843122d1636f
olgaoc <olgaodincova@gmail.com>
2023-12-31 15:48:41 +0200

    Serch bar functionality added, sorting added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,25 @@ import { Button } from 'antd';
 import styles from './header.module.css';
 
 const Header = () => {
+  // const tutorsSectionRef = useRef(null);
+
+  const handleClick = () => {
+    document.getElementById('tutorsSection').scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className={styles.headerWrapper}>
       <Container>
-      <div className={styles.logo}>tutorland</div>
+        <div className={styles.logo}>tutorland</div>
         <header className={styles.header}>
           <h1 className={styles.title}>Discover your learning journey with the best tutors</h1>
-           <Button type="primary" className={styles.button}>Try It</Button>
+          <Button
+            type="primary"
+            className={styles.button}
+            onClick={handleClick}
+          >
+            Get started
+          </Button>
         </header>
       </Container>
     </div>
--- src/components/tutors-section/tutors-section.js
+++ src/components/tutors-section/tutors-section.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
+import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 import Container from '../container/container';
-import { Button, Input, Space } from 'antd';
+import { Button, DatePicker, Input, Select, Space } from 'antd';
 import styles from './tutors-section.module.css';
 import StudyIcon from '../../assets/study_icon.png';
+// import SortImage from '../../assets/sort_icon.png';
 
 const TutorsSection = () => {
+  const [initialTutors, setInitialTutors] = useState([]);
   const [tutors, setTutors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedDate, setSelectedDate] = useState(null);
 
   useEffect(() => {
     getTutors();
@@ -15,35 +20,104 @@ const TutorsSection = () => {
   const getTutors = async () => {
     try {
       const response = await axios.get('https://tutors-back.onrender.com/tutors');
-      setTutors(response.data);
+      const tutorsData = response.data;
+      setInitialTutors(tutorsData);
+      setTutors(tutorsData);
       console.log(response.data);
     } catch (err) {
       console.log(err);
     }
   }
 
+  const handleSearch = () => {
+    const formattedDate = selectedDate ? dayjs(selectedDate).format('YYYY-MM-DD') : null;
+
+    
+    const filteredTutors = initialTutors.filter(tutor => {
+      const nameMatch = tutor.tutor_name.toLowerCase().includes(searchTerm.toLowerCase());
+      const surnameMatch = tutor.tutor_surname.toLowerCase().includes(searchTerm.toLowerCase());
+      const isAvailable = !tutor.lessons.some(entry => dayjs(entry.lesson_date).format('YYYY-MM-DD') === formattedDate);
+
+      return (nameMatch || surnameMatch) && isAvailable;
+    });
+
+    setTutors(filteredTutors);
+  };
+
+  const handleChange = (option) => {
+    switch (option) {
+      case 'sort':
+        setTutors(prev => [...prev].sort((a, b) => a.tutor_id - b.tutor_id));
+        break;
+      case 'name':
+        setTutors(prev => [...prev].sort((a, b) => a.tutor_name.localeCompare(b.tutor_name)));
+        break;
+      case 'price':
+        setTutors(prev => [...prev].sort((a, b) => a.hourly_price - b.hourly_price));
+        break;
+      case 'availability':
+        setTutors(prev => [...prev].sort((a, b) => a.lessons.length - b.lessons.length));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <Container>
-      <h2 className={styles.title}>Find your subject expert🚀</h2>
-      <Space.Compact style={{ width: '100%' }}>
-        <Input placeholder="Enter tutor name" />
-        <Button type="primary">Search</Button>
-      </Space.Compact>
-      <section className={styles.tutorsSection}>
-
-        {tutors.map(tutor => (
-          <div className={styles.tutorCard} key={tutor.tutor_id}>
-            <div className={styles.tutorNameWrapper}>
-              <div className={styles.tutorName}>{tutor.tutor_name} {tutor.tutor_surname}</div>
-              <div className={styles.tutorPrice}>{tutor.hourly_price} €/h</div>
-            </div>
-            <div className={styles.tutorSpecialization}>
-              <img className={styles.studyIcon} src={StudyIcon} alt='book icon' />
-              <div className={styles.discipline}>{tutor.tutor_specialization}</div>
-            </div>
-
+      <section className={styles.tutorsSectionWraper} id="tutorsSection">
+        <h2 className={styles.title}>Find your subject expert🚀</h2>
+        <div className={styles.search}>
+          <Space.Compact style={{ width: '60%' }}>
+            <Input
+              placeholder="Search tutor"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <DatePicker
+              placeholder="Select date"
+              onChange={(date) => setSelectedDate(date)}
+            />
+            <Button
+              type="primary"
+              onClick={handleSearch}
+            >
+              Search
+            </Button>
+          </Space.Compact>
+          <div className={styles.sortBar}>
+            <div className={styles.tutorsNumber}>{tutors.length} tutrs available</div>
+            {/* <Space.Compact> */}
+            {/* <img src={SortImage} alt='sort icon' /> */}
+            <Select
+              className={styles.sort}
+              defaultValue="sort"
+              style={{ width: 120 }}
+              onChange={handleChange}
+              options={[
+                { value: 'sort', label: 'Sort' },
+                { value: 'name', label: 'Name A-Z' },
+                { value: 'price', label: 'Lowest price' },
+                { value: 'availability', label: 'Availability' },
+              ]}
+            />
+            {/* </Space.Compact> */}
           </div>
-        ))}
+        </div>
+        <div className={styles.tutorsSection}>
+          {tutors.map(tutor => (
+            <div className={styles.tutorCard} key={tutor.tutor_id}>
+              <div className={styles.tutorNameWrapper}>
+                <div className={styles.tutorName}>{tutor.tutor_name} {tutor.tutor_surname}</div>
+                <div className={styles.tutorPrice}>{tutor.hourly_price} €/h</div>
+              </div>
+              <div className={styles.tutorSpecialization}>
+                <img className={styles.studyIcon} src={StudyIcon} alt='book icon' />
+                <div className={styles.discipline}>{tutor.tutor_specialization}</div>
+              </div>
+
+            </div>
+          ))}
+        </div>
       </section>
     </Container>
   )
